d200cf9933f595c263e73846c4f4b5f36e4a5b0d
Jeremy Welborn <jeremy.welborn@gmail.com>
2017-02-18 15:49:47 -0500

    set up db, started on schema

--- backend/init/init.js
+++
@@ -1,5 +0,0 @@
-// copied from dorm supplies api, code has not been touched
-
-console.log('running init script...');
-
-require('./createFirstAdmin');
--- backend_2/app.js
+++ backend_2/app.js
@@ -5,21 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-
-// <--
+// ?
 var users = require('./routes/users');
-// -->
+
+// ?
+var models = require('./models/index');
 
 var app = express();
 
-
-// <--
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-// -->
-
+var tempRouter = express.Router()
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,13 +21,22 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// <--
-app.use(express.static(path.join(__dirname, 'public')));
-// -->
 
+tempRouter.route('/pts')
+    .post(function(req, res) {
+        models.PT.create({
+            name: req.body.name
+        }).then(function(pt) {
+            res.json(pt);
+        });
+    })
+    .get(function(req, res) {
+        models.PT.findAll({}).then(function(PTs) {
+            res.json(PTs);
+        });
+    });
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', tempRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
