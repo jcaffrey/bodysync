bcd9ecd072e5f446eefe976a55ae8282001396de
Jeremy Welborn <jeremy.welborn@gmail.com>
2017-02-18 15:49:47 -0500

    set up db, started on schema

--- backend_2/models/pt.js
+++ backend_2/models/pt.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
     }, {
         classMethods: {
             associate: function(models) {
-                Patient.hasMany(models.Patient)
+                PT.hasMany(models.Patient)
             }
         }
     });
