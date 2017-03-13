5876e7e0d6dcd89b435dcab5a0392a630ee52659
Jeremy Welborn <jeremy.welborn@gmail.com>
2017-03-13 16:36:56 -0400

    factored out routes, working on requester-specific auth, wrapped up role-specific auth

--- backend/controllers/pts.js
+++ backend/controllers/pts.js
@@ -1,8 +1,13 @@
-var models = require('../models/index');
 // import instantiated db
+var models = require('../models/index');
+var jwt = require('jsonwebtoken');
+// app.locals.config = config not working?
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config/config.json')[env];  
 
 
-module.exports.createPT = (req, res, next) => {
+
+module.exports.createPt = (req, res, next) => {
     models.pt.create({
         name: req.body.name,
         email: req.body.email,
@@ -15,14 +20,36 @@ module.exports.createPT = (req, res, next) => {
     });
 };
 
-module.exports.getPTS = (req, res, next) => {
-    models.pt.findAll({}).then(function(PTs) {
-        res.json(PTs);
+module.exports.getPts = (req, res, next) => {
+    models.pt.findAll({}).then(function(pts) {
+        res.json(pts);
     });
 };
 
+/*
+function checkRequestIdAgainstId(req, res) { 
+    var token = req.query.token || req.body.token || req.headers['x-access-token'];
+    var decoded = jwt.verify(token, config.secret);
+    // debugging
+    console.log('token id: ' + String(decoded.id));
+    console.log('query id: ' + String(req.params.id));
+    if (req.params.id != decoded.id) {
+        return res.status(401).send('You are not authorized to see this resource');
+    }
+}
+*/
+module.exports.getPtById = (req, res, next) => {
+  
+    // already retrieved and verified the token, so skipping error handling for the time being
 
-module.exports.getPTById = (req, res, next) => {
+/*    var token = req.query.token || req.body.token || req.headers['x-access-token'];
+    var decoded = jwt.verify(token, 'thisisthesecret');
+    if (req.params.id != decoded.id) {
+        return res.status(401).send('You are not authorized to see this resource');
+    }
+*/
+    checkAgainstToken(req, res);
+
     models.pt.findAll({
         where: {
             id: req.params.id
@@ -33,7 +60,7 @@ module.exports.getPTById = (req, res, next) => {
 };
 
 
-module.exports.deletePT = (req, res, next) => {
+module.exports.deletePt = (req, res, next) => {
     models.pt.destroy({
         where: {
             id: req.params.id
@@ -46,7 +73,7 @@ module.exports.deletePT = (req, res, next) => {
     });
 }
 
-// module.exports.updatePT = (req, res, next) => {
+// module.exports.updatePt = (req, res, next) => {
 
 //     models.pt.udpate({
 //
