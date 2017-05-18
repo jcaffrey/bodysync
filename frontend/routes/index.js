5eb05d1ebc8e83a3893eafe13495ac9067ccbb92
Josh Seides <me@joshseides.com>
2017-05-18 18:25:32 -0400

    frontend routes added for exercises and exerciseSets

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -144,6 +144,47 @@ router.post('/romMetrics/:id/romMetricMeasures', function(req, res, next) {
 });
 
 // -------------------------------------------------------------------------------
+router.get('/injuries/:id/exerciseSets', function(req, res, next) {
+    request.get(config.apiUrl + '/injuries/' + req.params.id + '/exerciseSets?token=' + req.query.token, {
+        headers: {'x-access-token': req.query.token}
+    }).pipe(res);
+});
+
+router.get('/exerciseSets/:id', function(req, res, next) {
+    request.get(config.apiUrl + '/exerciseSets/' + req.params.id + '/?token=' + req.query.token, {
+        headers: {'x-access-token': req.query.token}
+    }).pipe(res);
+});
+
+router.get('/exerciseSets/:id/exercises', function(req, res, next) {
+    request.get(config.apiUrl + '/exerciseSets/' + req.params.id + '/exercises?token=' + req.query.token, {
+        headers: {'x-access-token': req.query.token}
+    }).pipe(res);
+});
+
+router.get('/exercises/:id', function(req, res, next) {
+    request.get(config.apiUrl + '/exercises/' + req.params.id + '/?token=' + req.query.token, {
+        headers: {'x-access-token': req.query.token}
+    }).pipe(res);
+});
+
+router.post('/injuries/:id/exerciseSets', function(req, res, next) {
+    request.post({
+        url: config.apiUrl + '/injuries/' + req.params.id + '/exerciseSets',
+        headers: {'x-access-token': req.headers['x-access-token']},
+        form: req.body
+    }).pipe(res);
+});
+
+router.post('/exerciseSets/:id/exercises', function(req, res, next) {
+    request.post({
+        url: config.apiUrl + '/exerciseSets/' + req.params.id + '/exercises',
+        headers: {'x-access-token': req.headers['x-access-token']},
+        form: req.body
+    }).pipe(res);
+});
+
+// -------------------------------------------------------------------------------
 router.get('/pt-form', function(req, res, next) {
     return res.render('pt-form', { firstName: 'Josh', footerButton: 'Cancel', footerButton2: 'Submit' });
 });
