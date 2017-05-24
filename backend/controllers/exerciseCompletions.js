e63e8af0652a6a1228df72064a3d6b22db7925c6
jcaffrey <jcaffrey@college.harvard.edu>
2017-05-24 16:14:24 -0400

    get route for exerciseCompletions

--- backend/controllers/exerciseCompletions.js
+++ backend/controllers/exerciseCompletions.js
@@ -77,124 +77,82 @@ module.exports.getCompletions = (req, res, next) => {
     var token = req.query.token || req.body.token || req.headers['x-access-token'];
     var decoded = jwt.verify(token, config.secret);
 
-    models.exerciseCompletion.findAll({
-        where: {
-            exerciseId: req.params.id
-        }
-    }).then(function (comps) {
-        if(comps.length !== 0) {
-            var firstComp = comps[0];
 
-            models.exercise.findOne({
-                where: {
-                    id: firstComp.exerciseId
+    models.exercise.findOne({where:{id:req.params.id}}).then(function(exer) {
+        if(Object.keys(exer).length !== 0)
+        {
+            models.exerciseCompletion.findAll({where:{exerciseId:exer.id}}).then(function (comps) {
+                if(comps.length !== 0)
+                {
+                    return res.json(comps);
                 }
-            }).then(function (exercise) {
-                console.log(exercise);
-                if(Object.keys(exercise).length !== 0) {
-                    models.exerciseSet.findOne({
-                        where: {
-                            id: exercise.exerciseSetId
-                        }
-                    }).then(function (set) {
-                        console.log(set);
-                        if(Object.keys(set).length !== 0) {
-                            if(decoded.isPt) {
-                                if(decoded.id == set.ptId) {
-                                    return res.json(comps);
-                                } else {
-                                    return res.status(401).send('PT unauthorized');
-                                }
-                            }
-                            // is patient
-                            else {
-                                if(!decoded.isPt) {
-                                    models.injury.findOne({
-                                        where: {
-                                            id: set.injuryId
-                                        }
-                                    }).then(function (injury) {
-                                        console.log(injury);
-                                        if(Object.keys(injury).length !== 0) {
-                                            if(decoded.id == injury.patientId) {
-                                                return res.json(comps);
-                                            } else {
-                                                return res.status(401).send('Patient unauthorized');
-                                            }
-                                        }
-                                    })
-                                }
-                            }
-                        }
-
-                    })
-                }
-
+            }).catch(function (err) {
+                return next(err);
             })
-        } else {
-            res.status(404).send('No stats associated with that exercise');
-        }
-    })
-}
-
-module.exports.getCompletionById = (req, res, next) => {
-    var token = req.query.token || req.body.token || req.headers['x-access-token'];
-    var decoded = jwt.verify(token, config.secret);
-
-    models.exerciseCompletion.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then(function(comp) {
-        if(Object.keys(comp).length !== 0) {
-            models.exercise.findOne({
-                where: {
-                    id: comp.exerciseId
-                }
-            }).then(function (exercise) {
-                if(Object.keys(exercise).length !== 0) {
-                    models.exerciseSet.findOne({
-                        where: {
-                            id: exercise.exerciseSetId
-                        }
-                    }).then(function (set) {
-                        if(Object.keys(set).length !== 0) {
-                            if(decoded.isPt) {
-                                if(decoded.id == set.ptId) {
-                                    return res.json(comp);
-                                } else {
-                                    return res.status(401).send('PT unauthorized');
-                                }
-
-                            }
-                            // is patient
-                            else {
-                                models.injury.findOne({
-                                    where: {
-                                        id: set.injuryId
-                                    }
-                                }).then(function (injury) {
-                                    if(Object.keys(injury).length !== 0) {
-                                        if(decoded.id == injury.patientId) {
-                                            return res.json(comp);
-                                        } else {
-                                            return res.status(401).send('Patient unauthorized');
-                                        }
-                                    }
-                                })
-                            }
-                        }
-                    })
-                }
-            })
-        }
-        else {
-            res.status(404).send('No stats associated with that exercise');
         }
     }).catch(function (err) {
         return next(err);
     })
 }
+
+// module.exports.getCompletionById = (req, res, next) => {
+//     var token = req.query.token || req.body.token || req.headers['x-access-token'];
+//     var decoded = jwt.verify(token, config.secret);
+//
+//     models.exerciseCompletion.findOne({
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(function(comp) {
+//         if(Object.keys(comp).length !== 0) {
+//             models.exercise.findOne({
+//                 where: {
+//                     id: comp.exerciseId
+//                 }
+//             }).then(function (exercise) {
+//                 if(Object.keys(exercise).length !== 0) {
+//                     models.exerciseSet.findOne({
+//                         where: {
+//                             id: exercise.exerciseSetId
+//                         }
+//                     }).then(function (set) {
+//                         if(Object.keys(set).length !== 0) {
+//                             if(decoded.isPt) {
+//                                 if(decoded.id == set.ptId) {
+//                                     return res.json(comp);
+//                                 } else {
+//                                     return res.status(401).send('PT unauthorized');
+//                                 }
+//
+//                             }
+//                             // is patient
+//                             else {
+//                                 models.injury.findOne({
+//                                     where: {
+//                                         id: set.injuryId
+//                                     }
+//                                 }).then(function (injury) {
+//                                     if(Object.keys(injury).length !== 0) {
+//                                         if(decoded.id == injury.patientId) {
+//                                             return res.json(comp);
+//                                         } else {
+//                                             return res.status(401).send('Patient unauthorized');
+//                                         }
+//                                     }
+//                                 })
+//                             }
+//                         }
+//                     })
+//                 }
+//             })
+//         }
+//         else {
+//             res.status(404).send('No stats associated with that exercise');
+//         }
+//     }).catch(function (err) {
+//         return next(err);
+//     })
+// }
 
 /**
 
@@ -211,51 +169,51 @@ module.exports.getCompletionById = (req, res, next) => {
  */
 
 
-module.exports.deleteCompletion = (req, res, next) => {
-    var token = req.query.token || req.body.token || req.headers['x-access-token'];
-    var decoded = jwt.verify(token, config.secret);
-
-    models.exerciseCompletion.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then(function (comp) {
-        if(Object.keys(comp).length !== 0) {
-            models.exercise.findOne({
-                where: {
-                    id: comp.exerciseId
-                }
-            }).then(function (exercise) {
-                if(Object.keys(exercise).length !== 0) {
-                    models.exerciseSet.findOne({
-                        where: {
-                            id: exercise.exerciseSetId
-                        }
-                    }).then(function (set) {
-                        if(Object.keys(set).length !== 0) {
-                            models.injury.findOne({
-                                where: {
-                                    id: set.injuryId
-                                }
-                            }).then(function (injury) {
-                                if(Object.keys(injury).length !== 0) {
-                                    if(!decoded.isPt && decoded.id == injury.patientId) {
-                                        comp.destroy();
-                                        return res.json(comp);
-                                    } else {
-                                        return res.status(401).send('Patient unauthorized');
-                                    }
-                                }
-                            })
-                        }
-
-                    })
-                }
-            })
-        } else {
-            res.status(404).send('No exercise with that id');
-        }
-    }).catch(function (err) {
-        return next(err);
-    })
-}
+// module.exports.deleteCompletion = (req, res, next) => {
+//     var token = req.query.token || req.body.token || req.headers['x-access-token'];
+//     var decoded = jwt.verify(token, config.secret);
+//
+//     models.exerciseCompletion.findOne({
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(function (comp) {
+//         if(Object.keys(comp).length !== 0) {
+//             models.exercise.findOne({
+//                 where: {
+//                     id: comp.exerciseId
+//                 }
+//             }).then(function (exercise) {
+//                 if(Object.keys(exercise).length !== 0) {
+//                     models.exerciseSet.findOne({
+//                         where: {
+//                             id: exercise.exerciseSetId
+//                         }
+//                     }).then(function (set) {
+//                         if(Object.keys(set).length !== 0) {
+//                             models.injury.findOne({
+//                                 where: {
+//                                     id: set.injuryId
+//                                 }
+//                             }).then(function (injury) {
+//                                 if(Object.keys(injury).length !== 0) {
+//                                     if(!decoded.isPt && decoded.id == injury.patientId) {
+//                                         comp.destroy();
+//                                         return res.json(comp);
+//                                     } else {
+//                                         return res.status(401).send('Patient unauthorized');
+//                                     }
+//                                 }
+//                             })
+//                         }
+//
+//                     })
+//                 }
+//             })
+//         } else {
+//             res.status(404).send('No exercise with that id');
+//         }
+//     }).catch(function (err) {
+//         return next(err);
+//     })
+// }
